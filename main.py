79bc9722530b7b8d95459a1033502666ddff17d6
gogodjames <jamesgogod@gmail.com>
2024-12-18 23:29:45 -0800

    Forking this to make a reddit bot

--- main.py
+++ main.py
@@ -1,11 +1,43 @@
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
-api_key = os.getenv("OPENAI_API_KEY")
+gpt_api_key = os.getenv("OPENAI_API_KEY")
+news_api_key = os.getenv("NEWS_API_KEY")
+REDDIT_CLIENT_ID = os.getenv('REDDIT_CLIENT_ID')
+REDDIT_CLIENT_SECRET = os.getenv('REDDIT_CLIENT_SECRET')
+REDDIT_USER_AGENT = os.getenv('REDDIT_USER_AGENT')
+REDDIT_USERNAME = os.getenv('REDDIT_USERNAME')
+REDDIT_PASSWORD = os.getenv('REDDIT_PASSWORD')
 
-import openai
-openai.api_key=api_key
+import praw
+redditbot = praw.Reddit(
+    client_id=REDDIT_CLIENT_ID,
+    client_secret=REDDIT_CLIENT_SECRET,
+    user_agent=REDDIT_USER_AGENT,
+    username=REDDIT_USERNAME,
+    password=REDDIT_PASSWORD
+)
+subreddit = redditbot.subreddit("politics")
+for submission in subreddit.hot(limit=10):
+    print(submission.title)
+
+'''from newsdataapi import NewsDataApiClient
+newsapi = NewsDataApiClient(apikey="pub_62754921c21b1816fa80ae62f8c39337f2a15")
+response = newsapi.latest_api(country='us',language='en', category='sports')
+for article in response['results']:
+    print(f"Title: {article['title']}")
+    print(f"Categories: {article['category']}")
+    print("---")'''
+
+'''from newsapi import NewsApiClient
+
+newsapi = NewsApiClient(news_api_key)
+headlines=newsapi.get_top_headlines(country='us',sortBy='popularity',page_size=1,category='general')
+for article in headlines['articles']:
+    print(article['title'])'''
+
+'''import openai
+openai.api_key=gpt_api_key
 response = openai.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
@@ -14,4 +46,4 @@ response = openai.chat.completions.create(
     ],
     max_tokens= 100
 )
-print(response['choices'][0]['message']['content'])
+print(response['choices'][0]['message']['content'])'''
